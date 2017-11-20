97c27ad65914ba52ab46441e816bcd17d4667435
Waldecy <waldecyfa@gmail.com>
2017-11-20 13:19:10 -0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/cadastrodeclientes/index.php';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'http://localhost/cadastrodeclientes/welcome/cadastro';
 
 /*
 |--------------------------------------------------------------------------
